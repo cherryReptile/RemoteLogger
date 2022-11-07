8571bd80c13b3e55085b188a65ab3d6ba79a0b43
cherryReptile <lisovskiy.nikitk@gmail.com>
2022-11-07 03:12:20 +0300

    test controller and route

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,27 @@
 package main
 
 import (
-	"fmt"
+	"github.com/pavel-one/GoStarter/internal/base"
+	"github.com/pavel-one/GoStarter/internal/controllers"
+	"log"
+	"os"
 )
 
 func main() {
-	fmt.Println("test")
-	fmt.Println("another test")
+	fatalChan := make(chan error, 1)
+
+	app := new(base.App)
+	app.Init()
+
+	testController := new(controllers.TestController)
+
+	app.Router.GET("/", testController.Test)
+	go app.Run("80", fatalChan)
+
+	err := <-fatalChan
+	if err != nil {
+		app.Close()
+		log.Printf("[FATAL] %v", err)
+		os.Exit(1)
+	}
 }
--- internal/controllers/Base.go
+++ internal/controllers/Base.go
@@ -1,1 +1,10 @@
 package controllers
+
+import "github.com/jmoiron/sqlx"
+
+type BaseController struct {
+}
+
+type DatabaseController struct {
+	DB *sqlx.DB
+}
