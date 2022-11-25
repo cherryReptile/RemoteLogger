89a0e5435310841ca8c71a218c219f6cb64f0213
cherryReptile <lisovskiy.nikitk@gmail.com>
2022-11-25 22:19:51 +0300

    request to github in helper

--- internal/helpers/helper.go
+++ internal/helpers/helper.go
@@ -2,6 +2,7 @@ package helpers
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 	"strings"
 )
 
@@ -13,4 +14,22 @@ func CheckAuthHeader(ctx *gin.Context) (string, bool) {
 	}
 
 	return authHeader[1], true
+}
+
+func RequestToGithub(token string) (*http.Response, error) {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", "https://api.github.com/user", nil)
+
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Authorization", "Bearer "+token)
+
+	res, err := client.Do(req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
 }
