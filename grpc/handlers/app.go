f65b961eaa9d73034148ec4395a6afaf29d07cbf
cherryReptile <lisovskiy.nikitk@gmail.com>
2022-12-25 06:22:47 +0300

    app new with new structure auth

--- grpc/handlers/app.go
+++ grpc/handlers/app.go
@@ -2,8 +2,13 @@ package handlers
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"github.com/jmoiron/sqlx"
 	"github.com/pavel-one/GoStarter/api"
+	"github.com/pavel-one/GoStarter/grpc/internal/appauth"
+	"github.com/pavel-one/GoStarter/grpc/internal/pgmodels"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type AppAuthService struct {
@@ -18,74 +23,108 @@ func NewAppAuthService(db *sqlx.DB) *AppAuthService {
 }
 
 func (a *AppAuthService) Register(ctx context.Context, req *api.AppRequest) (*api.AppResponse, error) {
-	//user := new(pgmodels.User)
-	//token := new(pgmodels.AccessToken)
-	//
-	//user.CheckOnExistsWithoutPassword(a.DB, req.Email, "app")
-	//if user.ID != "" {
-	//	return nil, errors.New("this user already exists")
-	//}
-	//
-	//hashP, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
-	//if err != nil {
-	//	return nil, err
-	//}
-	//
-	//user.Password = string(hashP)
-	//user.Login = req.Email
-	//
-	//err = user.Create(a.DB, "app")
-	//if err != nil {
-	//	return nil, err
-	//}
-	//
-	//if user.ID == "" {
-	//	return nil, err
-	//}
-	//
-	//tokenStr, err := appauth.GenerateToken(user.ID, user.Login, "app")
-	//if err != nil {
-	//	return nil, err
-	//}
-	//
-	//token.Token = tokenStr
-	//token.UserUUID = user.ID
-	//
-	//if err = token.Create(a.DB); err != nil {
-	//	return nil, err
-	//}
-	//
-	//return ToAppResponse(user, token), nil
-	return nil, nil
+	user := new(pgmodels.User)
+	ap := new(pgmodels.AuthProvider)
+	pd := new(pgmodels.ProvidersData)
+	token := new(pgmodels.AccessToken)
+
+	user.CheckOnExistsWithoutPassword(a.DB, req.Email, "app")
+	if user.ID != "" {
+		return nil, errors.New("this user already exists")
+	}
+
+	hashP, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
+
+	user.Login = req.Email
+
+	err = user.Create(a.DB, "app")
+	if err != nil {
+		return nil, err
+	}
+
+	if user.ID == "" {
+		return nil, err
+	}
+
+	ap.GetByProvider(a.DB, "app")
+	if ap.ID == 0 {
+		return nil, errors.New("unknown auth provider")
+	}
+
+	json, err := json.Marshal(map[string]string{"password": string(hashP)})
+	if err != nil {
+		return nil, err
+	}
+
+	pd.UserData = json
+	pd.UserID = user.ID
+	pd.ProviderID = ap.ID
+	if err = pd.Create(a.DB); err != nil {
+		return nil, err
+	}
+
+	tokenStr, err := appauth.GenerateToken(user.ID, user.Login, "app")
+	if err != nil {
+		return nil, err
+	}
+
+	token.Token = tokenStr
+	token.UserUUID = user.ID
+
+	if err = token.Create(a.DB); err != nil {
+		return nil, err
+	}
+
+	return ToAppResponse(user, token), nil
 }
 
 func (a *AppAuthService) Login(ctx context.Context, req *api.AppRequest) (*api.AppResponse, error) {
-	//user := new(pgmodels.User)
-	//token := new(pgmodels.AccessToken)
-	//
-	//if err := user.CheckOnExistsWithoutPassword(a.DB, req.Email, "app"); err != nil {
-	//	return nil, err
-	//}
-	//
-	//if user.ID == "" {
-	//	return nil, errors.New("user not found")
-	//}
-	//
-	//if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(req.Password)); err != nil {
-	//	return nil, err
-	//}
-	//
-	//tokenStr, err := appauth.GenerateToken(user.ID, user.Login, "app")
-	//if err != nil {
-	//	return nil, err
-	//}
-	//
-	//token.Token = tokenStr
-	//token.UserUUID = user.ID
-	//if err = token.Create(a.DB); err != nil {
-	//	return nil, err
-	//}
-	//
-	//return ToAppResponse(user, token), nil
-	return nil, nil
+	user := new(pgmodels.User)
+	ap := new(pgmodels.AuthProvider)
+	pd := new(pgmodels.ProvidersData)
+	token := new(pgmodels.AccessToken)
+
+	if err := user.CheckOnExistsWithoutPassword(a.DB, req.Email, "app"); err != nil {
+		return nil, err
+	}
+
+	if user.ID == "" {
+		return nil, errors.New("user not found")
+	}
+
+	ap.GetByProvider(a.DB, "app")
+	if ap.ID == 0 {
+		return nil, errors.New("unknown provider")
+	}
+
+	pd.FindByUserUUIDAndProviderID(a.DB, user.ID, ap.ID)
+	if pd.ID == 0 {
+		return nil, errors.New("user's provider data not found")
+	}
+
+	var data map[string]string
+	err := json.Unmarshal(pd.UserData, &data)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = bcrypt.CompareHashAndPassword([]byte(data["password"]), []byte(req.Password)); err != nil {
+		return nil, err
+	}
+
+	tokenStr, err := appauth.GenerateToken(user.ID, user.Login, "app")
+	if err != nil {
+		return nil, err
+	}
+
+	token.Token = tokenStr
+	token.UserUUID = user.ID
+	if err = token.Create(a.DB); err != nil {
+		return nil, err
+	}
+
+	return ToAppResponse(user, token), nil
 }
